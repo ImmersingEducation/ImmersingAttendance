4d2bff190bac1d989daefecabcd366447924488b
programmer-cc <2140130341@qq.com>
2026-05-31 21:22:29 +0800

    feat 添加一些必要方法

--- shared/src/commonMain/kotlin/top/iedu/iattendance/model/AttendanceRecord.kt
+++ shared/src/commonMain/kotlin/top/iedu/iattendance/model/AttendanceRecord.kt
@@ -9,4 +9,24 @@ data class AttendanceRecord(
     var editTime: LocalDateTime,
     var name: String,
     val situations: MutableMap<Student, AttendanceSituation>,
-)
+) {
+    fun peopleNeedAttendanceAmount(): Int {
+        return template.needAttendanceStudent.size
+    }
+
+    fun peopleActuallyAttendedAmount(): Int {
+        var cnt = 0
+        situations.forEach { (student, situation) ->
+            if (situation == AttendanceSituation.Arrived) cnt ++
+        }
+        return cnt
+    }
+
+    fun peopleActuallyLeavedAmount(): Int {
+        var cnt = 0
+        situations.forEach { (student, situation) ->
+            if (situation == AttendanceSituation.Leaved) cnt ++
+        }
+        return cnt
+    }
+}
--- shared/src/commonMain/kotlin/top/iedu/iattendance/model/AttendanceSituation.kt
+++ shared/src/commonMain/kotlin/top/iedu/iattendance/model/AttendanceSituation.kt
@@ -1,4 +1,9 @@
 package top.iedu.iattendance.model
 
 enum class AttendanceSituation {
+    Arrived, // 已到
+    NonArrival, // 未到
+    NoRequirements, // 无需参与
+    Late, // 迟到
+    Leaved // 请假
 }
